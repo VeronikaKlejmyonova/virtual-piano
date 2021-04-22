b5c1092a63845498fb9e35a0594ab40d258c845a
VeronikaKlejmyonova <veronika.klejmyonova@gmail.com>
2021-04-23 01:06:02 +0500

    feat: add click on a keyboard key

--- script.js
+++ script.js
@@ -12,6 +12,16 @@ function startSound(event) {
     audio.play();
 }
 
+// функция при использовании клавиатуры
+function playSound(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.piano-key[data-key="${event.keyCode}"]`);
+    if (!audio) return;
+    key.classList.add('active');
+    audio.currentTime = 0;
+    audio.play();
+}
+
 // удаление класса active
 const stopSound = (event) => {
     event.target.classList.remove("active");
@@ -24,6 +34,7 @@ const startCorrespondOver = (event) => {
         }
 
         COLLECTION.forEach((elem) => { // метод для перебора всех элементов массива COLLECTIO
+            //    window.addEventListener('keydown', playSound);
             elem.addEventListener("mousedown", startSound); // обработчик - на каждый элемент, когда мы на него нажимаем мышкой срабатывает функция startSound
             elem.addEventListener("mouseover", startSound); // обработчик - на каждый элемент, когда мы на него наводм мышкой срабатывает функция startSound 
             elem.addEventListener("mouseout", stopSound); // обработчик - на каждый элемент, когда мы с него убераем курсор срабатывает функция stopSound
@@ -35,11 +46,22 @@ const startCorrespondOver = (event) => {
 const stopCorrespondOver = () => {
     COLLECTION.forEach((elem) => { // метод для перебора всех элементов массива COLLECTIO 
         elem.classList.remove("active"); // удаление у элемента класс active - элемент не активный
+        //  elem.removeEventListener('mouseleave', stopSound);
+        elem.removeEventListener('keydown', stopSound);
         elem.removeEventListener("mouseout", stopSound); // когда мы убераем курсор с кнопки появл mouseout
         elem.removeEventListener("mouseover", startSound); // когда мы наводм на кнопку появл mouseover
     });
 }
 
-
+//mouseover навести курсором
+//mouseout убран курсор
+//mousedown Кнопка мыши нажата 
+//mouseup  Кнопка отпущена над элементом
+//mouseleave происходит, когда курсор покидает элемент
+//keydown  При  нажатии клавиши
+//keyup клавишу отпускают
 PIANO.addEventListener("mousedown", startCorrespondOver);
 document.addEventListener("mouseup", stopCorrespondOver);
+//PIANO.addEventListener("mouseleave", stopCorrespondOver);
+window.addEventListener('keydown', playSound);
+window.addEventListener('keyup', stopCorrespondOver);
