8c0b5b2fe43af7360fb2ce9a3a43bb01422808c3
VeronikaKlejmyonova <veronika.klejmyonova@gmail.com>
2021-04-28 15:25:30 +0500

    feat: add a fullscreen button

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ function playSound(event) {
 const stopSound = (event) => {
     event.target.classList.remove("active");
 }
-
 const startCorrespondOver = (event) => {
         if (event.target.classList.contains("piano-key")) // если мой таргет будет содержать класс piano-key
         { // если это кнопка, то
@@ -65,3 +64,36 @@ document.addEventListener("mouseup", stopCorrespondOver);
 //PIANO.addEventListener("mouseleave", stopCorrespondOver);
 window.addEventListener('keydown', playSound);
 window.addEventListener('keyup', stopCorrespondOver);
+
+
+// FULLSCREEN
+const bt = document.querySelector('#fullscreen');
+
+bt.addEventListener('click', event => {
+    event = document.documentElement;
+    if (event.requestFullscreen) {
+        event.requestFullscreen(); // W3C spec
+    } else if (event.mozRequestFullScreen) {
+        event.mozRequestFullScreen(); // Firefox
+    } else if (event.webkitRequestFullscreen) {
+        event.webkitRequestFullscreen(); // Safari
+    } else if (event.msRequestFullscreen) {
+        event.msRequestFullscreen(); // IE/Edge
+    }
+
+    if (document.fullscreenElement) {
+        deactivateFullscreen();
+    }
+});
+
+///function activateFullscreen(event) {};
+
+function deactivateFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    }
+};
